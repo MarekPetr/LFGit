1f0d975876ce3962482902fcda0959a80358ea09
Petr Marek <petr.marek18@gmail.com>
2020-02-01 13:54:41 +0100

    Link annex directly - not working either

--- app/src/main/java/com/lfgit/tasks/GitAnnexExec.java
+++ app/src/main/java/com/lfgit/tasks/GitAnnexExec.java
@@ -3,6 +3,7 @@ package com.lfgit.tasks;
 import android.app.Activity;
 
 import static com.lfgit.Constants.binDir;
+import static com.lfgit.Constants.filesDir;
 import static com.lfgit.Constants.usrDir;
 
 public class GitAnnexExec extends Executor {
@@ -16,7 +17,7 @@ public class GitAnnexExec extends Executor {
 
     public String annex() {
         //envExeForRes("git-annex", "", "init");
-        envExeForRes("shimmed/git-annex/git-annex", "", "init");
+        envExeForRes("exe/git-annex", "clone", "--library-path", usrDir + "lib/aarch64-linux-gnu:"+ usrDir + "lib:" + usrDir + "etc/ld.so.conf.d:" + usrDir + "usr/lib/aarch64-linux-gnu/audit:" + usrDir + "usr/lib/aarch64-linux-gnu/gconv", usrDir + "shimmed/git-annex/git-annex", "init");
         return getResult();
     }
 
