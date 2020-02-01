291475182b0b328c2f4899ae94ca88b91e139b15
Petr Marek <petr.marek18@gmail.com>
2020-02-01 13:32:41 +0100

    proot working, still no output from annex

--- app/src/main/java/com/lfgit/tasks/GitAnnexExec.java
+++ app/src/main/java/com/lfgit/tasks/GitAnnexExec.java
@@ -3,18 +3,20 @@ package com.lfgit.tasks;
 import android.app.Activity;
 
 import static com.lfgit.Constants.binDir;
+import static com.lfgit.Constants.usrDir;
 
 public class GitAnnexExec extends Executor {
 
     public GitAnnexExec( Activity activity) {
         super(activity);
-        exe = binDir;
+        exe = usrDir;
     }
 
 
 
     public String annex() {
-        envExeForRes("git-annex", "");
+        //envExeForRes("git-annex", "", "init");
+        envExeForRes("shimmed/git-annex/git-annex", "", "init");
         return getResult();
     }
 
